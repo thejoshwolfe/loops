8afd0dcbeb4d0e6e9d47ce21150cb4042f086b0d
Josh Wolfe <thejoshwolfe@gmail.com>
2023-02-11 15:58:26 -0500

    update to typescript 4

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-npm install || exit 1
-
-ALL_THE_OPTIONS_PLEASE='--removeComments --strict --noImplicitAny --strictNullChecks --strictFunctionTypes --strictPropertyInitialization --noImplicitThis --alwaysStrict --noUnusedLocals --noUnusedParameters --noImplicitReturns --noFallthroughCasesInSwitch'
-
-./node_modules/.bin/tsc --lib es2015,DOM,ES5,ScriptHost ${ALL_THE_OPTIONS_PLEASE} public/a.ts || exit 1
+npm install && ./node_modules/.bin/tsc
